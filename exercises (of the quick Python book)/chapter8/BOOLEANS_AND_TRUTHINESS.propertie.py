72bcceb3a1b9381e5ca13593bac4ce2167ee8785
AminRezaei <4min.Rezaei@gmail.com>
2022-08-09 19:41:24 +0430

    all exercises of chapter 8 has been added.

---
+++ exercises (of the quick Python book)/chapter8/BOOLEANS_AND_TRUTHINESS.propertie.py
@@ -0,0 +1,16 @@
+'''
+Decide whether the following statements are true or false:
+1, 0, -1, [0], 1 and 0, 1 > 0 or []
+'''
+print(bool(1))
+# output: True
+print(bool(0))
+# output: False
+print(bool(-1))
+# output: True
+print(bool([0]))
+# output: True
+print(bool(1 and 0))
+# output: False
+print(bool(1 > 0 or []))
+# output: True
